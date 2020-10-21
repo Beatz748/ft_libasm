f33e11093212a45b6519194cc63568eb1b187b0a
UGF3ZWwgTW9za2Fsw7xr TW9za2Fsw7xr <kshantel@ga-u1.msk.21-school.ru>
2020-10-21 17:49:09 +0300

    fd

--- ft_strdup.s
+++ ft_strdup.s
@@ -5,18 +5,23 @@ section .text
 	extern _ft_strcpy
 
 _ft_strdup:
+	cmp	rdi, 0
+	jz _ret_null
 	push rdi
 	call _ft_strlen
+	inc rax
 	mov rdi, rax
 	call _malloc
 	cmp rax, 0
-	je _exit
-	pop rsi
-	mov rdi, rax
-	sub rsp, 8
+	jz _malloc_error
+	pop	rsi
+	mov	rdi, rax
 	call _ft_strcpy
-	add rsp, 8
-	jmp _exit
+	ret
 
-_exit:
+_malloc_error:
+	pop	rdi
+	ret
+
+_ret_null:
 	ret
